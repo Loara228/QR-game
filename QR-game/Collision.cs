99addcf748fa0fefe3b55a727869f529222e631b
Loara228 <my.zero.ife@gmail.com>
2025-01-15 23:38:02 +0500

    коллизия для пуль

--- QR-game/Collision.cs
+++ QR-game/Collision.cs
@@ -10,7 +10,64 @@ namespace QR_game
 {
     internal static class Collision
     {
-        public static bool ObjObj(PhysicsObject obj1, PhysicsObject obj2)
+
+        #region world
+
+        public static IEnumerable<T> GetObjects<T>() where T : GameObj =>
+            Game1.CurrentLevel.Objects.OfType<T>();
+
+        public static IEnumerable<T> Trace<T>(Vector2 start, Vector2 end) where T : GameObj
+        {
+            var result = new List<T();
+            foreach (var obj in GetObjects<T>())
+            {
+                if (LineObj(start, end, obj))
+                {
+                    result.Add(obj);
+                }
+            }
+            return result.Cast<T>();
+        }
+
+        public static bool LineLine(Vector2 start1, Vector2 end1, Vector2 start2, Vector2 end2)
+        {
+            // calculate the direction of the lines
+            float uA = ((end2.X - start2.X) * (start1.Y - start2.Y) - (end2.Y - start2.Y) * (start1.X - start2.X)) / ((end2.Y - start2.Y) * (end1.X - start1.X) - (end2.X - start2.X) * (end1.Y - start1.Y));
+            float uB = ((end1.X - start1.X) * (start1.Y - start2.Y) - (end1.Y - start1.Y) * (start1.X - start2.X)) / ((end2.Y - start2.Y) * (end1.X - start1.X) - (end2.X - start2.X) * (end1.Y - start1.Y));
+
+            // if uA and uB are between 0-1, lines are colliding
+            if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
+            {
+                // point where the lines meet
+                Vector2 intersectionPoint = new Vector2(
+                    start1.X + (uA * (end1.X - start1.X)),
+                    start1.Y + (uA * (end1.Y - start1.Y)));
+                return true;
+            }
+            return false;
+        }
+
+        public static bool LineObj(Vector2 start, Vector2 end, GameObj obj)
+        {
+            // check if the line has hit any of the rectangle's sides
+            // uses the Line/Line function below
+            bool left = LineLine(start, end, obj.Rect.TopLeft, obj.Rect.BottomLeft);
+            bool right = LineLine(start, end, obj.Rect.TopRight, obj.Rect.BottomRight);
+            bool top = LineLine(start, end, obj.Rect.TopLeft, obj.Rect.TopRight);
+            bool bottom = LineLine(start, end, obj.Rect.BottomLeft, obj.Rect.BottomRight);
+
+            // if ANY of the above are true, the line
+            // has hit the rectangle
+            if (left || right || top || bottom)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+
+        public static bool ObjObj(GameObj obj1, GameObj obj2)
         {
             return obj1.Rect.Right >= obj2.X &&
                     obj1.X <= obj2.Rect.Right &&
@@ -18,7 +75,7 @@ namespace QR_game
                     obj1.Y <= obj2.Rect.Bottom;
         }
 
-        public static bool CircleObj(Vector2 center, float radius, PhysicsObject obj)
+        public static bool CircleObj(Vector2 center, float radius, GameObj obj)
         {
             // temporary variables to set edges for testing
             float testX = center.X;
